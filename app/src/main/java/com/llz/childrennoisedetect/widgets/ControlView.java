7832fa4cd1a232afc573308628a65ee61f776e97
383455526@qq.com <383455526@qq.com>
2016-01-29 18:13:14 +0800

    添加控制按钮自定义控件

--- app/src/main/java/com/llz/childrennoisedetect/widgets/ControlView.java
+++ app/src/main/java/com/llz/childrennoisedetect/widgets/ControlView.java
@@ -1,9 +1,19 @@
 package com.llz.childrennoisedetect.widgets;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
 /**
  * Created by ysbang on 2016/1/29.
  */
-public class ControlView {
-    public ControlView() {
+public class ControlView extends LinearLayout
+{
+    public ControlView(Context context) {
+        this(context,null);
+    }
+
+    public ControlView(Context context,AttributeSet attrs) {
+        super(context,attrs);
     }
 }
